1121c6f9ef1ece002bd956dddb99dbfd2c28ee4c
Mandar Chitre <mandar@arl.nus.edu.sg>
2016-08-13 16:25:56 +0800

    Cleaned up demo1

--- samples/demo1.py
+++ samples/demo1.py
@@ -10,97 +10,69 @@ import arlpy.signal as asig
 from arlpy import comms
 from arlpy.utils import db2mag, mag2db, pow2db
 
-## BPSK
+## settings
 
-def tx_bpsk(d):
-    return comms.modulate(d, comms.psk())
+tests = [
+    { 'name': 'BPSK', 'const': comms.psk(2) },
+    { 'name': 'QPSK', 'const': comms.psk(4) },
+    { 'name': 'BFSK', 'const': comms.fsk(2) }
+]
 
-def rx_bpsk(x):
-    return comms.demodulate(x, comms.psk())
+ebn0 = range(0, 21, 1)
+bits = 100000
 
-bpsk = { 'name': 'BPSK', 'tx': tx_bpsk, 'rx': rx_bpsk, 'm': 2, 'n': 1 }
-
-## QPSK
-
-def tx_qpsk(d):
-    return comms.modulate(d, comms.psk(4))
-
-def rx_qpsk(x):
-    return comms.demodulate(x, comms.psk(4))
-
-qpsk = { 'name': 'QPSK', 'tx': tx_qpsk, 'rx': rx_qpsk, 'm': 4, 'n': 1 }
-
-## BFSK
-
-def tx_bfsk(d):
-    return comms.modulate(d, comms.fsk())
-
-def rx_bfsk(x):
-    return comms.demodulate(x, comms.fsk())
-
-bfsk = { 'name': 'BFSK', 'tx': tx_bfsk, 'rx': rx_bfsk, 'm': 2, 'n': 4 }
-
-## MSK
-
-def tx_msk(d):
-    return comms.modulate(d, comms.msk())
-
-def rx_msk(x):
-    return comms.demodulate(x, comms.msk())
-
-msk = { 'name': 'MSK', 'tx': tx_msk, 'rx': rx_msk, 'm': 4, 'n': 4 }
-
-## tests
-
-tests = [bpsk, qpsk, bfsk, msk]
-
-## run tests
-
+pb = False
 fs = 108000
 fd = 18000
 fc = 27000
-fstop = 0.6*fd
-ebn0 = range(0, 21, 1)
-bits = 100000
-pb = False
-pulse = comms.rrcosfir(0.25, fs/fd)
 
-def run_test(t, ebn0=range(21), syms=1000000):
+## run tests
+
+if pb:
+    assert fs % fd == 0
+    fs_by_fd = fs/fd
+    pulse = comms.rrcosfir(0.25, fs_by_fd)
+    pulse_delay = (len(pulse)-1)/2/fs_by_fd
+
+def run_test(const, ebn0=range(21), syms=1000000):
+    m = len(const)
+    n = const.shape[1] if const.ndim == 2 else 1
     ber = []
     for s in ebn0:
         # eb is divided across n samples, and each symbol has m bits
-        snr_per_sample = s + pow2db(np.log2(t['m'])/t['n'])
-        d1 = comms.random_data(syms, t['m'])
-        x = t['tx'](d1)
+        snr_per_sample = s + pow2db(np.log2(m)/n)
+        d1 = comms.random_data(syms, m)
+        x = comms.modulate(d1, const)
         if pb:
-            x = comms.upconvert(x, fs/fd, fc, fs, pulse)
-            # 3 dB extra SNR needed to account for conjugate noise spectrum
+            x = comms.upconvert(x, fs_by_fd, fc, fs, pulse)
+            # 3 dB extra SNR per sample needed to account for conjugate noise spectrum
             x = comms.awgn(x, snr_per_sample+3, complex=False)
-            x = comms.downconvert(x, fs/fd, fc, fs, pulse)
-            delay = (len(pulse)-1)/(fs/fd)
-            x = x[delay:-delay]
+            x = comms.downconvert(x, fs_by_fd, fc, fs, pulse)
+            x = x[2*pulse_delay:-2*pulse_delay]
         else:
             x = comms.awgn(x, snr_per_sample, complex=True)
-        d2 = t['rx'](x)
-        ber.append(comms.ber(d1, d2, t['m']))
+        d2 = comms.demodulate(x, const)
+        ber.append(comms.ber(d1, d2, m))
     return ebn0, ber
-
-def Q(x):
-    return 0.5*scipy.special.erfc(x/np.sqrt(2))
 
 plt.figure()
 
 names = []
 for t in tests:
-    ebn0, ber = run_test(t, ebn0, int(bits/np.log2(t['m'])))
+    ebn0, ber = run_test(t['const'], ebn0, int(bits/np.log2(len(t['const']))))
     plt.semilogy(ebn0, ber, '*')
     names.append(t['name'])
 
+## theory
+
+def Q(x):
+    return 0.5*scipy.special.erfc(x/np.sqrt(2))
+
 ebn0 = np.arange(0, 21, 0.1)
-plt.plot(ebn0, Q(db2mag(ebn0+3)), '--')
+plt.plot(ebn0, Q(np.sqrt(2)*db2mag(ebn0)), '--')
 names.append('BPSK/QPSK theory')
-plt.plot(ebn0, 2*Q(db2mag(ebn0)), '--')  # FIXME: fudge factor of 2, why???
-names.append('MSK theory')
+plt.plot(ebn0, Q(db2mag(ebn0)), '--')
+names.append('BFSK theory')
 
 plt.axis([0, 20, 1e-5, 0.5])
 plt.xlabel('Eb/N0 (dB)')
