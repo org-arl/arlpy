73c56da924cba9bfe59ab7d7068901437864b3cd
Mandar Chitre <mandar@arl.nus.edu.sg>
2016-08-15 22:24:54 +0800

    Updated PLL demo code - WIP

--- samples/dpll1.py
+++ samples/dpll1.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# demo program to demo PLL
+# demo program to test PLL
 ##########################################################################
 
 import numpy as np
@@ -15,18 +15,19 @@ x = comms.modulate(comms.random_data(1000, 2), comms.psk(2))
 x = comms.upconvert(x, 60, fc, fs, comms.rrcosfir(0.25, 60))
 
 b1, a1 = sp.iirfilter(4, (0.98*2*fc/(fs/2), 1.02*2*fc/(fs/2)))
-b2, a2 = sp.iirfilter(4, 0.01, btype='lowpass')
-f1 = asig.lfilter_gen(b1, a1)
+b2, a2 = sp.iirfilter(4, 0.001, btype='lowpass')
+#f1 = asig.lfilter_gen(b1, a1)
 f2 = asig.lfilter_gen(b2, a2)
 nco = asig.nco_gen(2*fc, fs, phase0=np.pi) #, phase0=2*np.pi*54000*5/fs)
 
 nco.next()
 e2 = np.empty_like(x)
-y = np.empty_like(x)
+#y = np.empty_like(x)
+y = sp.lfilter(b1, a1, x**2)
 z = np.empty_like(x, dtype=np.complex)
 for j in xrange(len(x)):
-    y[j] = f1.send(x[j]**2)
-    z[j] = nco.send(2*fc+1e6*(e2[j-1] if j >= 1 else 0))
-    e2[j] = y[j]*np.real(z[j])
+    #y[j] = f1.send(x[j]**2)
+    z[j] = nco.send(2*fc+1e4*(e2[j-1] if j >= 1 else 0))
+    e2[j] = f2.send(y[j]*np.real(z[j]))
 
 print np.mean(e2[3000:-3000])
