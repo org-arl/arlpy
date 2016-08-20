d43adf81dc245ecdbb048e5733ac96485da799f7
Mandar Chitre <mandar@arl.nus.edu.sg>
2016-08-21 00:42:12 +0800

    Added linspace3 for completeness

--- arlpy/utils.py
+++ arlpy/utils.py
@@ -28,15 +28,15 @@ def db2pow(x):
     """Convert dB quantity to power."""
     return _np.power(10, x/10.0)
 
-def linspace2d(start1, stop1, num1, start2, stop2, num2):
+def linspace2d(start0, stop0, num0, start1, stop1, num1):
     """Generate linearly spaced coordinates in 2D space.
 
-    :param start1: first value on axis 0
-    :param stop1: last value on axis 0
-    :param num1: number of values on axis 0
-    :param start2: first value on axis 1
-    :param stop2: last value on axis 1
-    :param num2: number of values on axis 1
+    :param start0: first value on axis 0
+    :param stop0: last value on axis 0
+    :param num0: number of values on axis 0
+    :param start1: first value on axis 1
+    :param stop1: last value on axis 1
+    :param num1: number of values on axis 1
 
     >>> from arlpy import bf
     >>> bf.linspace2d(0, 1, 2, 0, 1, 3)
@@ -47,6 +47,33 @@ def linspace2d(start1, stop1, num1, start2, stop2, num2):
      [ 1. ,  0.5],
      [ 1. ,  1. ]]
     """
-    x = _np.linspace(start1, stop1, num1, dtype=_np.float)
-    y = _np.linspace(start2, stop2, num2, dtype=_np.float)
+    x = _np.linspace(start0, stop0, num0, dtype=_np.float)
+    y = _np.linspace(start1, stop1, num1, dtype=_np.float)
     return _np.array(_np.meshgrid(x, y)).T.reshape(-1, 2)
+
+def linspace3d(start0, stop0, num0, start1, stop1, num1, start2, stop2, num2):
+    """Generate linearly spaced coordinates in 2D space.
+
+    :param start0: first value on axis 0
+    :param stop0: last value on axis 0
+    :param num0: number of values on axis 0
+    :param start1: first value on axis 1
+    :param stop1: last value on axis 1
+    :param num1: number of values on axis 1
+    :param start2: first value on axis 2
+    :param stop2: last value on axis 2
+    :param num2: number of values on axis 2
+
+    >>> from arlpy import bf
+    >>> bf.linspace3d(0, 1, 2, 0, 1, 3, 0, 0, 1)
+    [[ 0. ,  0. , 0. ],
+     [ 0. ,  0.5, 0. ],
+     [ 0. ,  1. , 0. ],
+     [ 1. ,  0. , 0. ],
+     [ 1. ,  0.5, 0. ],
+     [ 1. ,  1. , 0. ]]
+    """
+    x = _np.linspace(start0, stop0, num0, dtype=_np.float)
+    y = _np.linspace(start1, stop1, num1, dtype=_np.float)
+    z = _np.linspace(start2, stop2, num2, dtype=_np.float)
+    return _np.array(_np.meshgrid(x, y, z)).T.reshape(-1, 3)
