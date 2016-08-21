417a1af906af71763b8f2ff41d8d0942ea99648d
Mandar Chitre <mandar@arl.nus.edu.sg>
2016-08-21 16:46:47 +0800

    Refactored beamforming API

--- arlpy/bf.py
+++ arlpy/bf.py
@@ -13,13 +13,6 @@
 import numpy as _np
 import scipy.signal as _sig
 
-# beamformer mode constants
-BARTLETT = 'bartlett'
-CAPON = 'capon'
-
-# beamformer output constants
-POWER = 'power'
-
 def normalize(x):
     """Normalize array time series data to be zero-mean and unit variance.
 
@@ -55,8 +48,8 @@ def stft(x, nfft, overlap=0, window=None):
     x = _np.fft.fft(x, axis=1)
     return x
 
-def steer(pos, theta, wavelength=2, nfft=1, fs=None, fc=None):
-    """Generate steering vectors.
+def steering(pos, theta):
+    """Compute steering distances.
 
     For linear arrays, pos is 1D array. For planar and 3D arrays, pos is a 2D array with a
     sensor position vector in each row.
@@ -66,35 +59,25 @@ def steer(pos, theta, wavelength=2, nfft=1, fs=None, fc=None):
     Such arrays can be easily generated using the :func:`arlpy.utils.linspace2d` function.
     The broadside direction is along the z-axis, and has azimuth and elevation as 0.
 
-    If the wavelength is not specified, it is assumed that the position vectors are normalized
-    to units of half-wavelength.
-
-    If nfft > 1 then fs and fc must be specified for broadband beamforming.
-
     :param pos: sensor positions
     :param theta: steering directions
-    :param wavelength: wavelength at carrier frequency
-    :param nfft: STFT window length in samples
-    :param fs: sampling rate
-    :param fc: carrier frequency
-    :returns: steering column vectors for each direction
+    :returns: steering distances with a column for each direction
 
     >>> import numpy as np
     >>> from arlpy import bf, utils
     >>> pos1 = [0.0, 0.5, 1.0, 1.5, 2.0]
-    >>> a1 = bf.steer(pos1, np.deg2rad(np.linspace(-90, 90, 181)))
+    >>> a1 = bf.steering(pos1, np.deg2rad(np.linspace(-90, 90, 181)))
     >>> pos2 = [[0.0, 0.0],
                 [0.0, 0.5],
                 [0.5, 0.0],
                 [0.5, 0.5]]
-    >>> a2 = bf.steer(pos2, np.deg2rad(utils.linspace2d(-20, 20, 41, -10, 10, 21)))
+    >>> a2 = bf.steering(pos2, np.deg2rad(utils.linspace2d(-20, 20, 41, -10, 10, 21)))
     """
     pos = _np.asarray(pos, dtype=_np.float)
-    pos /= wavelength/2.0
     theta = _np.asarray(theta, dtype=_np.float)
     if pos.ndim == 1:
         pos -= _np.mean(pos)
-        phase = _np.pi * pos[:,_np.newaxis] * _np.sin(theta)
+        dist = pos[:,_np.newaxis] * _np.sin(theta)
     else:
         if pos.shape[1] != 2 and pos.shape[1] != 3:
             raise ValueError('Sensor positions must be either 2d or 3d vectors')
@@ -104,62 +87,102 @@ def steer(pos, theta, wavelength=2, nfft=1, fs=None, fc=None):
         azim = theta[:,0]
         elev = theta[:,1]
         dvec = _np.array([-_np.sin(azim)*_np.cos(elev), _np.sin(elev), _np.cos(azim)*_np.cos(elev)])
-        phase = _np.pi * _np.dot(pos, dvec)
-    if nfft > 1:
-        phase2 = _np.empty((phase.shape[0], nfft, phase.shape[1]), dtype=_np.complex)
-        for i in range(nfft):
-            phase2[:,i,:] = phase * (1+float(i*fs)/(nfft*fc))
-        phase = phase2
-    return _np.exp(-1j * phase)/_np.sqrt(pos.shape[0])
+        dist = _np.dot(pos, dvec)
+    return dist
 
-def _beamform(x, a, mode):
-    # narrowband beamformer
-    assert x.shape[1] == a.shape[1]
-    # TODO
+def bartlett(x, fc, c, sd, complex_output=False):
+    """Bartlett beamformer.
 
-def beamform(x, a, mode=BARTLETT, out=POWER):
-    """Beamform array data.
-
-    For narrowband beamforming, the array data must be 2D with baseband time series for each
-    sensor in individual columns. The steering vectors must also be 2D with a column per
+    The array data must be 2D with baseband time series for each sensor in
+    individual columns. The steering vectors must also be 2D with a column per
     steering direction, as produced by the :func:`steering` function.
 
-    For broadband beamforming, the array data must be 3D with time, frequency and sensors as
-    the dimensions, as produced by the :func:`stft` function. The steering vectors must also be
-    3D, as produced by the :func:`steering` function.
+    The data is assumed to be narrowband. If broadband beamforming is desired, use the
+    :func:`broadband` function instead.
 
     :param x: array data
-    :param a: steering vectors
-    :param mode: beamformer mode (BARTLETT or CAPON)
-    :param out: output type (POWER or COMPLEX)
-    :returns: beamformer output with time as the first axis, and steering directions as the last axis
-
-    In case of broadband beamforming with COMPLEX output, the beamformer output has frequency as
-    the middle axis. In case of broadband beamforming with POWER output, the power is integrated
-    across the entire bandwidth.
+    :param fc: carrier frequency for the array data
+    :param c: wave propagation speed
+    :param sd: steering distances
+    :param complex_output: True for complex signal, False for beamformed power
+    :returns: beamformer output with time as the first axis, and steering directions as the other
 
     >>> from arlpy import bf
-    >>> # broadband time series array data assumed to be loaded in x
-    >>> # with sampling rate fs and carrier frequency fc
+    >>> import numpy as np
+    >>> # narrowband (1 kHz) time series array data assumed to be loaded in x
     >>> # sensor positions assumed to be in pos
-    >>> a = bf.steering(pos, np.linspace(-np.pi/2, np.pi/2, 181), nfft=256, fs=fs, fc=fc)
-    >>> y = bf.beamform(bf.stft(x, 256), a, mode=bf.CAPON)
+    >>> y = bf.bartlett(x, 1000, 1500, bf.steering(pos, np.linspace(-np.pi/2, np.pi/2, 181)))
     """
-    if mode != BARTLETT and mode != CAPON:
-        raise ValueError('Invalid beamformer mode')
-    if out != POWER and out != COMPLEX:
-        raise ValueError('Invalid output type')
-    if x.shape[-1] != a.shape[0]:
+    if x.shape[1] != sd.shape[0]:
         raise ValueError('Sensor count mismatch in data and steering vector')
-    # handle narrowband data
-    if len(x.shape) == 2:
-        bfo = _beamform(x, a, mode)
-        return _np.abs(bfo)**2 if out == POWER else bfo
-    # handle broadband data
-    m, nfft, n = x.shape
-    if nfft != a.shape[1]:
-        raise ValueError('Frequency bin mismatch in data and steering vector')
-    bfo = _np.empty((m, nfft, a.shape[0]))
-    for f in range(nfft):
-        bfo[:,f,:] = _beamform(x[:,f,:], a[:,f,:], mode)
-    return _np.sum(_np.abs(bfo)**2, axis=1) if out == POWER else bfo
+    wavelength = float(c)/fc
+    a = _np.exp(-2j*_np.pi*sd/wavelength)/_np.sqrt(sd.shape[1])
+    bfo = _np.dot(x, a.conj())
+    return bfo if complex_output else _np.abs(bfo)**2
+
+def capon(x, fc, c, sd, complex_output=False):
+    """Capon beamformer.
+
+    The array data must be 2D with baseband time series for each sensor in
+    individual columns. The steering vectors must also be 2D with a column per
+    steering direction, as produced by the :func:`steering` function.
+
+    The data is assumed to be narrowband. If broadband beamforming is desired, use the
+    :func:`broadband` function instead.
+
+    :param x: array data
+    :param fc: carrier frequency for the array data
+    :param c: wave propagation speed
+    :param sd: steering distances
+    :param complex_output: True for complex signal, False for beamformed power
+    :returns: beamformer output with time as the first axis, and steering directions as the other
+
+    >>> from arlpy import bf
+    >>> import numpy as np
+    >>> # narrowband (1 kHz) time series array data assumed to be loaded in x
+    >>> # sensor positions assumed to be in pos
+    >>> y = bf.capon(x, 1000, 1500, bf.steering(pos, np.linspace(-np.pi/2, np.pi/2, 181)))
+    """
+    if x.shape[1] != sd.shape[0]:
+        raise ValueError('Sensor count mismatch in data and steering vector')
+    wavelength = float(c)/fc
+    a = _np.exp(-2j*_np.pi*sd/wavelength)/_np.sqrt(sd.shape[1])
+    # TODO compute w
+    bfo = _np.dot(x, w.conj())
+    return bfo if complex_output else _np.abs(bfo)**2
+
+def broadband(x, fs, c, nfft, sd, f0=0, beamformer=bartlett):
+    """Broadband beamformer.
+
+    The broadband beamformer is implementing by taking STFT of the data, applying narrowband
+    beamforming to each frequency bin, and integrating the beamformer output power across
+    the entire bandwidth.
+
+    The array data must be 2D with baseband time series for each sensor in
+    individual columns. The steering vectors must also be 2D with a column per
+    steering direction, as produced by the :func:`steering` function.
+
+    The STFT window size should be chosen such that the corresponding distance (based on wave
+    propagation speed) is much larger than the aperture size of the array.
+
+    :param x: array data
+    :param fs: sampling rate for array data
+    :param c: wave propagation speed
+    :param nfft: STFT window size
+    :param sd: steering distances
+    :param f0: carrier frequency (for baseband data)
+    :param beamformer: narrowband beamformer to use
+    :returns: beamformer output with time as the first axis, and steering directions as the other
+
+    >>> from arlpy import bf
+    >>> # passband time series array data assumed to be loaded in x, sampled at fs
+    >>> # sensor positions assumed to be in pos
+    >>> sd = bf.steering(pos, np.linspace(-np.pi/2, np.pi/2, 181))
+    >>> y = bf.broadband(x, fs, 256, sd, beamformer=capon)
+    """
+    x = stft(x, nfft)
+    bfo = _np.zeros((x.shape[0], sd.shape[0]))
+    for i in range(nfft):
+        f = f0 + i * float(fs)/nfft
+        bfo += beamformer(x[:,i,:], f, c, sd, complex_output=False)
+    return bfo
