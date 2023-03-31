5ba7be0e4ef0f27ea9dca74d2c6ac5ccfb991329
Ivana Escobar <ivana@utexas.edu>
2023-03-31 14:52:34 -0500

    bellhop.exe in $PATH

--- arlpy/uwapm.py
+++ arlpy/uwapm.py
@@ -624,7 +624,7 @@ class _Bellhop:
 
     def _bellhop(self, *args):
         try:
-            _proc.run(f'./bellhop.exe {" ".join(list(args))}', 
+            _proc.run(f'bellhop.exe {" ".join(list(args))}', 
                       stderr=_proc.STDOUT, stdout=_proc.PIPE,
                       shell=True)
         except OSError:
