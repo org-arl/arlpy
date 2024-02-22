ccd958c956687c990956762d2b0b4069dbc5e414
Philip Lundrigan <lundrigan@byu.edu>
2024-02-21 19:24:56 -0700

    Add tests

--- tests/context.py
+++ tests/context.py
@@ -6,6 +6,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from arlpy import utils
 from arlpy import geo
 from arlpy import uwa
+from arlpy import uwapm
 from arlpy import signal
 from arlpy import comms
 from arlpy import bf
